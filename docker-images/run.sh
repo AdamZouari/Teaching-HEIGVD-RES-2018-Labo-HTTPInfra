7aad6b7c80c0998e72dab3076be152a4b64d3c79
Unknown <james.smith@heig-vd.ch>
2018-06-11 10:13:10 +0200

    add script for management-ui

--- docker-images/run.sh
+++ docker-images/run.sh
@@ -45,3 +45,7 @@ docker inspect express_dynamic1 | grep -i ipaddress
 
 echo "\n### check ip express_dynamic2 conatiner\n"
 docker inspect express_dynamic2 | grep -i ipaddress
+
+#Source : https://portainer.io/install.html
+docker volume create portainer_data
+docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
